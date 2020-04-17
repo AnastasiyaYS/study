88a4b9af9a07c842e2c81853c7b64c55275cd738
Anastasiya <Nastya199529@mail.ru>
2020-04-18 01:42:29 +0400

    solve exercises of arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,24 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |item| item.positive? ? array.max : item }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        iter = lambda do |array_part, index|
+          length = array_part.length
+          return -1 if length.zero?
+
+          middle_index = length / 2
+          middle_item = array_part[middle_index]
+          if query == middle_item
+            middle_index + index
+          elsif query < middle_item
+            iter.call(array_part[0, middle_index], index)
+          else iter.call(array_part[middle_index + 1, middle_index], index + middle_index + 1)
+          end
+        end
+        iter.call(array, 0)
       end
     end
   end
