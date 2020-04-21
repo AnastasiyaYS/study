0efc7fdcddd5c9090c0e938b2bf50d8d9fa6f77c
Anastasiya <Nastya199529@mail.ru>
2020-04-21 18:36:45 +0400

    move a function max to class level and rewrite this

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,18 +1,14 @@
 module Exercise
   module Arrays
     class << self
+      def max(array)
+        max = array.first
+        array.each { |item| max = item if item > max }
+        max
+      end
+
       def replace(array)
-        max = lambda do |full_array|
-          iter = lambda do |part_of_array, max_number|
-            return max_number if part_of_array.empty?
-
-            max_number = max_number < part_of_array[0] ? part_of_array[0] : max_number
-            iter.call(part_of_array[1, part_of_array.length - 1], max_number)
-          end
-          iter.call(full_array, full_array[0])
-        end
-        max_number = max.call(array)
-
+        max_number = max(array)
         array.map { |item| item.positive? ? max_number : item }
       end
 
